8e0ea1dd0de8f5cab948f08bb848ec5dc3470df4
Quý <150776876+VanQuy07@users.noreply.github.com>
2025-05-02 01:13:18 +0700

    giải quyết conflict khi merge

--- DieRoll.java
+++ DieRoll.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.logging.ConsoleHandler.level = INFO;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
